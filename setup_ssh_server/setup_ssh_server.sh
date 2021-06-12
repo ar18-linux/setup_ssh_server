e673bea44405b0e38e4030267865b16c2de869a7
nulysses <adr.zivil@gmail.com>
2021-06-12 12:04:30 +0200

    1

--- setup_ssh_server/setup_ssh_server.sh
+++ setup_ssh_server/setup_ssh_server.sh
@@ -5,8 +5,9 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 set -ex
 LD_PRELOAD=
 
+rm -rf "${script_dir}/helper_functions"
 git clone https://github.com/ar18-linux/helper_functions.git
-. "${script_dir}/helper_functions/helper_functions.sh"
+. "${script_dir}/helper_functions/helper_functions/helper_functions.sh"
 
 obtain_sudo_password
 
