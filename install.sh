f32c91c34afb072a56469cb99f3220b1b119213f
nulysses <adr.zivil@gmail.com>
2021-06-12 14:52:03 +0200

    1

--- install.sh
+++ install.sh
@@ -3,14 +3,13 @@
 # Template version 2021-06-12.01
 LD_PRELOAD=
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-#IFS=$'\n' shell_options=($(shopt -op))
-set -ex
-#set -u
-#set -o pipefail
+IFS=$'\n' shell_options=($(shopt -op))
+set -exu
+set -o pipefail
 # Start of script
 
 . "${script_dir}/vars"
-if [ -z "${ar18_helper_functions+x}" ]; then rm -rf "/tmp/helper_functions"; cd /tmp; git clone https://github.com/ar18-linux/helper_functions.git; . "/tmp/helper_functions/helper_functions/helper_functions.sh"; cd "${script_dir}"; fi
+if [ -z "${ar18_helper_functions+x}" ]; then rm -rf "/tmp/helper_functions_$(logname)"; cd /tmp; git clone https://github.com/ar18-linux/helper_functions.git; mv "/tmp/helper_functions" "/tmp/helper_functions_$(logname)" . "/tmp/helper_functions_$(logname)/helper_functions/helper_functions.sh"; cd "${script_dir}"; fi
 
 obtain_sudo_password
 
